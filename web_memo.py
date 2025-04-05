4a251d127a5d08f254612328de2e33e405cc27c4
kwsktech <popsteel1969@gmail.com>
2025-04-06 00:08:25 +0900

    Update web_memo.py

--- web_memo.py
+++ web_memo.py
@@ -130,3 +130,9 @@ if __name__ == "__main__":
     init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+from flask import send_from_directory
+
+@app.route('/manifest.json')
+def manifest():
+    return send_from_directory('.', 'manifest.json')
